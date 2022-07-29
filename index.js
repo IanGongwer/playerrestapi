c266ddf424acdc6d1edd72132a37ae23cbdfb670
Ian Gongwer <iangongwerpersonal@gmail.com>
2022-07-29 13:52:25 -0400

    added post method

--- index.js
+++ index.js
@@ -12,20 +12,16 @@ let port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello World");
-})
+});
 
 app.get("/apiData", (req, res) => {
     res.send(importData);
-})
+});
 
 app.post("/apiData", (req, res) => {
-    let objectData = {
-        "name": req.body.name,
-        "id": req.body.id
-    };
     res.send("Success");
-})
+});
 
 app.listen(port, () => {
     console.log("API app listening on port 3000");
-})
+});
