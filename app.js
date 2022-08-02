ecf0289e4a26e9224e3671bf93d9a34b625b1765
Ian Gongwer <iangongwerpersonal@gmail.com>
2022-08-02 00:38:08 -0400

    creates pool instead of connection

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ const db = mysql.createPool({
     connectionLimit: 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     port: "3306"
 });
